a8ed9b1e0795eb5f88dcf40e092553dff913d58d
Felipe Duellis Padilha <felipedpadilha@gmail.com>
2025-06-30 21:29:30 -0300

    validation and errors for store method

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,29 +1,47 @@
 @extends('layouts.app')
 
-@section('title', 'Create Task')
+@section('title', 'Add Task')
+
+@section('styles')
+    <style>
+        .error-message {
+            color: red;
+            font-size: 0, 8rem;
+        }
+    </style>
+@endsection
 
 @section('content')
     <form method="POST" action="{{ route('tasks.store') }}">
-      @csrf
-      <div>
-        <label for="title">
-            Title
-        </label>
-        <input type="text" name="title" id="title" />
-      </div>
+        @csrf
+        <div>
+            <label for="title">
+                Title
+            </label>
+            <input type="text" name="title" id="title" />
+            @error('title')
+                <p class='error-message'>{{ $message }}</p>
+            @enderror
+        </div>
 
-      <div>
-        <label>Description</label>
-        <textarea name="description" id="description" rows="5"></textarea>
-      </div>
+        <div>
+            <label>Description</label>
+            <textarea name="description" id="description" rows="5"></textarea>
+            @error('description')
+                <p class='error-message'>{{ $message }}</p>
+            @enderror
+        </div>
 
-      <div>
-        <label>Long description</label>
-        <textarea name="long_description" id="long_description" rows="10"></textarea>
-      </div>
+        <div>
+            <label>Long description</label>
+            <textarea name="long_description" id="long_description" rows="10"></textarea>
+            @error('long_description')
+                <p class='error-message'>{{ $message }}</p>
+            @enderror
+        </div>
 
-      <div>
-        <button type="submit">Add Task</button>
-      </div>
+        <div>
+            <button type="submit">Add Task</button>
+        </div>
     </form>
 @endsection
