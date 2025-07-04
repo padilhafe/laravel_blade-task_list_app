daf9a16a334ed7aec6201ae76f1acd8adf42fa78
Felipe Duellis Padilha <felipedpadilha@gmail.com>
2025-07-03 21:57:43 -0300

    feat: destroy route

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,11 @@ Route::put('/tasks/{task}', function (TaskRequest $request, Task $task) {
         ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
 
+Route::delete('/tasks/{task}', function(Task $task) {
+    $task->delete();
+    return redirect()->route('tasks.index')->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
+
 Route::fallback(function () {
     return 'Still got somewhere!';
 });
