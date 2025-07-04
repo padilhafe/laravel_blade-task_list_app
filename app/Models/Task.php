a41cb843e5d3dde6e3434512797d8c94c1543d66
Felipe Duellis Padilha <felipedpadilha@gmail.com>
2025-07-03 22:23:01 -0300

    feat: complete a task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -14,4 +14,10 @@ class Task extends Model
         'description',
         'long_description',
     ];
+
+    public function toggleComplete()
+    {
+        $this->completed = !$this->completed;
+        $this->save();
+    }
 }
