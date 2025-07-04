17500de7dd6653f209af780caf7fa1ed55817a77
Felipe Duellis Padilha <felipedpadilha@gmail.com>
2025-07-03 22:56:03 -0300

    feat: index and show simple styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,35 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel 12 Task List App</title>
-        @yield('styles')
-    </head>
 
-    <body>
-        <h1>@yield('title')</h1>
-        <div>
-            @if (session()->has('success'))
-                <div>{{ session('success') }}</div>
-            @endif
-            @yield('content')
-        </div>
-    </body>
+<head>
+    <title>Laravel 12 Task List App</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    {{-- blade-formatter-disable --}}
+    <style type="text/tailwindcss">
+        .btn {
+            @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50
+        }
+        
+        .link {
+            @apply font-medium text-gray-700 underline decoration-pink-500
+        }
+    </style>
+    {{-- blade-formatter-enable --}}
+
+    @yield('styles')
+</head>
+
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+    <h1 class="text-2xl mb-4">
+        @yield('title')
+    </h1>
+    <div>
+        @if (session()->has('success'))
+            <div>{{ session('success') }}</div>
+        @endif
+        @yield('content')
+    </div>
+</body>
+
 </html>
